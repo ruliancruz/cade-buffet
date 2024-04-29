99e51741e646c728acc2a96a15e889e25991605a
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-04-29 10:37:39 -0300

    add client validations and unit tests

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,12 @@ class Client < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,
+            :cpf,
+            presence: true
+
+  validates :cpf, length: { is: 11 }
+  validates :cpf, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cpf, uniqueness: true
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,4 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
+  describe '#valid?' do
+    it 'false when name is blank' do
+      client = Client.new name: ''
+
+      client.valid?
+
+      expect(client.errors.full_messages
+        .include? 'Nome não pode ficar em branco').to be true
+    end
+
+    it 'false when cpf is blank' do
+      client = Client.new cpf: ''
+
+      client.valid?
+
+      expect(client.errors.full_messages
+        .include? 'CPF não pode ficar em branco').to be true
+    end
+
+    it "false when cpf isn't a number" do
+      client = Client.new cpf: 'qwertyuiopa'
+
+      client.valid?
+
+      expect(client.errors.full_messages
+        .include? 'CPF não é um número').to be true
+    end
+
+    it "false when cpf have less than 11 digits" do
+      client = Client.new cpf: '1234567890'
+
+      client.valid?
+
+      expect(client.errors.full_messages
+        .include? 'CPF não possui o tamanho esperado (11 caracteres)').to be true
+    end
+
+    it "false when cpf have more than 11 digits" do
+      client = Client.new cpf: '123456789012'
+
+      client.valid?
+
+      expect(client.errors.full_messages
+        .include? 'CPF não possui o tamanho esperado (11 caracteres)').to be true
+    end
+
+    it "false when cpf isn't unique" do
+      Client.create! name: 'User',
+                     cpf: '11480076015',
+                     email: 'client@example.com',
+                     password: 'password'
+
+      client = Client.new cpf: '11480076015'
+
+      client.valid?
+
+      expect(client.errors.full_messages.include? 'CPF já está em uso').to be true
+    end
+  end
 end
